cc5f62d73a128315955f8f96c3f9f20cc33ed5ce
HuyDQ_184271 <62065172+herohthd@users.noreply.github.com>
2021-04-01 21:38:46 +0700

    update route to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,21 @@
 
 import Nav from './components/Nav'
-import {BrowserRouter as Router} from 'react-router-dom';
 import SlideShow from './components/SlideShow'
 import Body from './components/Body'
 import Footer from './components/Footer'
+import Login from './components/Login'
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom'
 function App() {
   return (
     <Router>
     <div className="App">
       <header className="App-header">
         <Nav/>
+        <Switch>
+          <Route path="/Register" exact component ={Login}/> 
+          <Route path="/Login" exact component ={Login}/> 
+          <Route path="/Submit" exact component ={Login}/> 
+        </Switch>
         <SlideShow/>
         {/* <FixedNav/> */}
       </header>
