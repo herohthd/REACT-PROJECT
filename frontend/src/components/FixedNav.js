58ffc3b87767d486bb5b0fdd9200dc62ec410ac3
herohthd <62065172+herohthd@users.noreply.github.com>
2021-04-14 16:45:22 +0700

    Update FixedNav.js

--- frontend/src/components/FixedNav.js
+++ frontend/src/components/FixedNav.js
@@ -46,7 +46,7 @@ const FixedNav = () => {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav mr-auto">
                         <li className="nav-item" active>
-                            <a className="nav-link" href="/home">Home</a>
+                            <a className="nav-link" href="/">Home</a>
                         </li>
                         <li className="nav-item" active>
                             <a className="nav-link" href="/recipies">Recipies</a>
