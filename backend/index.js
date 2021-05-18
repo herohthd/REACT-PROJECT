f27254333a52e88b25f9ac39e8d930e944984ce2
herohthd <62065172+herohthd@users.noreply.github.com>
2021-05-18 20:27:35 +0700

    Version 1.0.0

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const routesHandler = require('./routes/handler.js');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 require('dotenv/config');
 
 mongoose.connect(process.env.DB_URI, {
@@ -24,6 +25,15 @@ app.use(cors());
 app.use('/',routesHandler);
 
 const PORT = process.env.PORT || 4000;
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('../frontend/build'));
+
+    app.get('*',(req,res) => {
+        res.sendFile(path.resolve(__dirname,'../frontend','build','index.html'));
+    });
+}
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 })
