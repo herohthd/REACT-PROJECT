79aa63bac32dd980eb8fa323b966e48cb5a115f1
herohthd <62065172+herohthd@users.noreply.github.com>
2021-05-19 09:08:32 +0700

    change index

--- index.js
+++ index.js
@@ -26,12 +26,13 @@ app.use('/',routesHandler);
 
 const PORT = process.env.PORT || 4000;
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("frontend/build"));
-    app.get("/*", function(req, res) {
-      res.sendFile(path.join(__dirname, "./frontend/build/index.html"));
-    });
-  }
+if(process.env.NODE_ENV === 'production'){
+    //set static folder
+    app.use(express.static('client/build'));
+}
+app.get('*',(req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
