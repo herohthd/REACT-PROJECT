a1ade76769247a07e6a91c99911f3335225598e7
herohthd <62065172+herohthd@users.noreply.github.com>
2021-06-03 16:40:18 +0700

    Link to Ha page

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -9,11 +9,11 @@ class Footer extends React.Component {
                 <div className="footer-content flex flex-jc-sb flex-ai-c">
                 <h4>Food Recipe Website - Group 3 - Web Programming</h4>
                 <ul className="flex flex-jc-c flex-ai-c">
-                    <li><Link to="https://www.facebook.com/ha.hoangthuy.9" className="link"><i className="fa fa-facebook" aria-hidden="true"></i></Link></li>
-                    <li><Link to="https://www.facebook.com/ha.hoangthuy.9" className="link"><i className="fa fa-google-plus" aria-hidden="true"></i></Link></li>
-                    <li><Link to="https://www.facebook.com/ha.hoangthuy.9" className="link"><i className="fa fa-instagram" aria-hidden="true"></i></Link></li>
-                    <li><Link to="https://www.facebook.com/ha.hoangthuy.9" className="link"><i className="fa fa-twitter" aria-hidden="true"></i></Link></li>
-                    <li><Link to="https://www.facebook.com/ha.hoangthuy.9" className="link"><i className="fa fa-youtube" aria-hidden="true"></i></Link></li>
+                    <li><Link to="/https://www.facebook.com/ha.hoangthuy.9/" className="link"><i className="fa fa-facebook" aria-hidden="true"></i></Link></li>
+                    <li><Link to="/https://www.facebook.com/ha.hoangthuy.9/" className="link"><i className="fa fa-google-plus" aria-hidden="true"></i></Link></li>
+                    <li><Link to="/https://www.facebook.com/ha.hoangthuy.9/" className="link"><i className="fa fa-instagram" aria-hidden="true"></i></Link></li>
+                    <li><Link to="/https://www.facebook.com/ha.hoangthuy.9/" className="link"><i className="fa fa-twitter" aria-hidden="true"></i></Link></li>
+                    <li><Link to="/https://www.facebook.com/ha.hoangthuy.9/" className="link"><i className="fa fa-youtube" aria-hidden="true"></i></Link></li>
                 </ul>
                 </div>
             </div>
